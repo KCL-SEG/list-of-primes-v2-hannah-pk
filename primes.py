42028b11e04de2dd58d26887b5a7f772809e21dd
Hannah Porteous <hannah.porteous@kcl.ac.uk>
2023-09-23 19:53:02 +0100

    Change primes.py as required

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("The number of primes should be 1 or greater")
     list = []
+    y = 1
+    while len(list) < 10:
+        y +=1
+        if is_prime(y,list):
+            list.append(y)
     return list
+
+def is_prime(number,primes):
+    for item in primes:
+        if number % item == 0:
+            return False
+    return True
